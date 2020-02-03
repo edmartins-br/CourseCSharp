271d74d27b0677e6894a01ce836764d49f711ba7
Eduardo Martins <edmartins_br@outlook.com>
2020-02-03 16:53:56 -0300

    Projeto XADREZ criado - Classe Posição!

---
+++ ws-vs2019/Xadrez-console/Xadrez-console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tabuleiro;
+
+namespace Xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+        }
+    }
+}
