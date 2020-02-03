9982b9cfefd79f2bb2139806249b6029217db5c2
Eduardo Martins <edmartins_br@outlook.com>
2020-02-03 11:35:08 -0300

    Exceptions

---
+++ ws-vs2019/Exeptions/HotelReservation/HotelReservation/HotelReservation/Entities/Reservation.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using HotelReservation.Entities.Exceptions;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservation.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Ceck-out date must be after Check-In date!");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                // Quando cai no Throw, corta o método, nem executa o que está abaixo
+                throw new DomainException("Reservation dates for update must be FUTURE DATES!");
+            }
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Ceck-out date must be after Check-In date!");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room: "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/mm/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/mm/yyyy")
+                + ", "
+                + Duration()
+                + " nights.";
+        }
+
+
+    }
+}
---
+++ ws-vs2019/Exeptions/HotelReservation/HotelReservation/HotelReservation/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using HotelReservation.Entities;
+using HotelReservation.Entities.Exceptions;
+namespace HotelReservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                Console.WriteLine("*=*=*=*=*=*=*= HOTEL RESERVATION SYSTEM *=*=*=*=*=*=*=");
+
+                Console.WriteLine("ENTER THE RESERVATION DATA");
+
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Check-in date (dd/mm/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-out date (dd/mm/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+                Console.WriteLine("Enter data to UpDate the Reservation: ");
+
+                Console.Write("Check-In date (dd/mm/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-Out date (dd/mm/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservarion: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            // tipo mais generico de exceção
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+        }
+    }
+}
