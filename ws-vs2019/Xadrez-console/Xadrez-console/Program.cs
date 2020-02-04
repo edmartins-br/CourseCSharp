020ec6e12c72732129d243daba03cad3f709a1b8
Eduardo Martins <edmartins_br@outlook.com>
2020-02-03 21:38:54 -0300

    Tratando erros com execoes personalizadas

--- ws-vs2019/Xadrez-console/Xadrez-console/Program.cs
+++ ws-vs2019/Xadrez-console/Xadrez-console/Program.cs
@@ -8,16 +8,23 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 7));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 4));
+                //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                Tela.imprimirTabuleiro(tab);
 
-            Tela.imprimirTabuleiro(tab);
-
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine("ERRO: " + e.Message);
+            }
             /*
             Posicao P;
             P = new Posicao(3, 4);
--- ws-vs2019/Xadrez-console/Xadrez-console/Tabuleiro/Tabuleiro.cs
+++ ws-vs2019/Xadrez-console/Xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using tabuleiro;
 
 namespace tabuleiro
 {
@@ -25,10 +26,49 @@ namespace tabuleiro
             return pecas[linha, coluna];
         }
 
+        // checa se a peca existe
+
+            public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        //sobrecarga do metodo Peca
+        // primeira melhoria
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        // coloca a peca no tabuleiro
+        // segunda melhoria
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        // verifica se a posição é valida
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if(!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Inválida!");
+            }
         }
     }
 }
