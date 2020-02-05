3afb2809a27537e6ff20d4f193cba0e5c8571b87
Eduardo Machado Martins <edmartins_br@outlook.com>
2020-02-05 13:27:48 -0300

    Trabalhando com arquivos - System.IO

---
+++ ws-vs2019/Arquivos/SystemIO/SystemIO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace SystemIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\EDUARDO\Desktop\Curso de C# - Udemy\ws-vs2019\Arquivos\SystemIO\file1.txt";
+            string targetPath = @"C:\Users\EDUARDO\Desktop\Curso de C# - Udemy\ws-vs2019\Arquivos\SystemIO\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath); // Le o caminho do arquivo
+                fileInfo.CopyTo(targetPath); // Faz uam cópia do arquivo para o target
+                string[] lines = File.ReadAllLines(sourcePath); // Guarda todas as linhas do arquivo num vetor
+                
+                foreach(string line in lines)// Imprime as linhas do vetor na tela
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
