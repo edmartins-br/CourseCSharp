f263e35f4750d106ec8c32352329047b9e919844
Eduardo Machado Martins <edmartins_br@outlook.com>
2020-02-05 15:38:12 -0300

    Instanciando File Stream e Stream Reader em duas linhas

--- ws-vs2019/Arquivos/SystemIO/SystemIO/Program.cs
+++ ws-vs2019/Arquivos/SystemIO/SystemIO/Program.cs
@@ -7,6 +7,37 @@ namespace SystemIO
     {
         static void Main(string[] args)
         {
+
+            string path = @"C:\Users\EDUARDO\Desktop\Curso de C# - Udemy\ws-vs2019\Arquivos\SystemIO\file1.txt";
+            
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                // estes recursos são externos e não são processados no .NET
+                // É preciso fechar os recursos manualmente de forma explicita e manual
+
+                fs = new FileStream(path, FileMode.Open); // instancia o file stream 
+                sr = new StreamReader(fs);
+
+                string line = sr.ReadLine();
+
+                Console.WriteLine(line);
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro: ");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if(sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+
+            // ================ FILE INFO / FILE =================
+            /*
             string sourcePath = @"C:\Users\EDUARDO\Desktop\Curso de C# - Udemy\ws-vs2019\Arquivos\SystemIO\file1.txt";
             string targetPath = @"C:\Users\EDUARDO\Desktop\Curso de C# - Udemy\ws-vs2019\Arquivos\SystemIO\file2.txt";
 
@@ -26,6 +57,7 @@ namespace SystemIO
                 Console.WriteLine("An error occured");
                 Console.WriteLine(e.Message);
             }
+            */
         }
     }
 }
