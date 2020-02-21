76acc2d74ccccb3efd94040d30a25725f2a2692c
Eduardo Machado Martins <edmartins_br@outlook.com>
2020-02-21 14:38:53 -0300

    Aula de Interface - Exemplo SEM utilizar interface

---
+++ ws-vs2019/Interface/Locadora/Locadora/Entities/CarRental.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Locadora.Entities
+{
+    class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null; //não é necessário pq automaticamente recebe nulo se não declarar nada
+        }
+    }
+}
